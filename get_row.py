d1f740288d1a65698724d05c90b7498f13ef727b
JamesYu5421 <sae-hyun@hotmail.com>
2018-04-19 19:10:40 -0700

    fixed some bugs

--- get_row.py
+++ get_row.py
@@ -6,38 +6,31 @@ import pymysql as sql
 
 class getRow(Resource):
     def __init__(self):
-        user_ = 'root'  # i just put the connect and db access information because I was using a local database
-        host_ = 'localhost'
-        pass_ = 'star2222'
-        db_ = 'derived'
-        conn = sql.connect(user=user_, host=host_, db=db_, passwd=pass_)
-        cursor = conn.cursor()
+        self.user_ = 'root'  # i just put the connect and db access information because I was using a local database
+        self.host_ = 'localhost'
+        self.pass_ = 'star2222'
+        self.db_ = 'derived'
 
     def get(self, row_params):
-        def __init__(self):
-            self.user_ = 'root'  # i just put the connect and db access information because I was using a local database
-            self.host_ = 'localhost'
-            self.pass_ = 'star2222'
-            self.db_ = 'derived'
+        if row_params is not None:
+            try:
+                print('started')
+                conn = sql.connect(user=self.user_, host=self.host_, db=self.db_, passwd=self.pass_)
+                print('connected')
+                cursor = conn.cursor()
+                # assuming that the url is something like: "http://something.com/get_results/Predicate/Subject/Object"
+                vals = row_params.split("+")
+                subject_ = vals[0]
+                predicate_ = vals[1]
+                object_ = vals[2]
+                where_query = "WHERE PREDICATE = '" + predicate_ + "' AND SUBJECT_NAME = '" + subject_ + "' AND OBJECT_NAME = '" + object_ + "'"
+                query_row_string = "SELECT * FROM austin_pred_occ_test " + where_query + " LIMIT 1;"
+                print("this is the query: " + query_row_string)
+                cursor.execute(query_row_string)
+                row = cursor.fetchall()
+                # I gotta find the last coloumn and print the result of the last coloumn
+                reponse = Response(json.dumps(row), status=200, mimetype='application/json')
+                return reponse
 
-        def get(self, row_params):
-            if row_params is not None:
-                try:
-                    conn = sql.connect(user=self.user_, host=self.host_, db=self.db_, passwd=self.pass_)
-                    cursor = conn.cursor()
-                    # assuming that the url is something like: "http://something.com/get_results/Predicate+Subject+Object"
-                    vals = row_params.split("+")
-                    subject_ = vals[0]
-                    predicate_ = vals[1]
-                    object_ = vals[2]
-                    where_query = "WHERE PREDICATE = '" + predicate_ + "' AND SUBJECT_NAME = '" + subject_ + "' AND OBJECT_NAME = '" + object_ + "'"
-                    query_row_string = "SELECT * FROM austin_pred_occ_test " + where_query + " LIMIT 1;"
-                    print(query_row_string)
-                    cursor.execute(query_row_string)
-                    row = cursor.fetchall()
-                    # I gotta find the last coloumn and print the result of the last coloumn
-                    reponse = Response(json.dumps(row), status=200, mimetype='application/json')
-                    return reponse
-
-                except:
-                    return json.dumps({})
+            except:
+                return json.dumps({})
