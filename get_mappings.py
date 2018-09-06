ee559a2acd6c88318c06c39c5364b390f63bb337
amichne <austinmichne@gmail.com>
2018-09-05 19:08:27 -0700

    Code review - needs testing

--- get_mappings.py
+++ get_mappings.py
@@ -1,69 +1,111 @@
 import json
+from dataclasses import dataclass
+from typing import List, T, Dict
+from enum import Enum
 from flask_restful import Resource
-from flask import Flask, Response
+from flask import Flask, Response, request
 #import MySQLdb as sql
 import pymysql as sql
 
+
+class ConceptType(Enum):
+    COND = "Condition"
+    OBS = "Observation"
+    TRT = "Treatment"
+
+
+class EncodingSystem(Enum):
+    LOINC = "LOINC"
+    SNMD = "SNOMED"
+    RX = "RxNorm"
+
+
+class Relation(Enum):
+    TRTS = "0"
+
+
+@dataclass
 class concept:
-    def __init__(self, type, code, system, value, units):
-        self.concept_type = type
+    concept_type: ConceptType
+    concept_code: str = ""
+    encoding_system: EncodingSystem
+    concept_value: float = 0.0
+    concept_units: str = ""
+
+    def __init__(self, type_, code, system, value, units):
+        self.concept_type = type_
         self.concept_code = code
-        self.coding_system = system
+        self.encoding_system = system
         self.concept_value = value
         self.concept_units = units
 
 
+@dataclass
 class mapping:
+    concept1_hash: str = ""
+    concept2_hash: str = ""
+    relation: Relation
+    coeff_dict: Dict = None
+
     def __init__(self, hash1, hash2, relation, coeff_dict):
         self.concept1_hash = hash1
         self.concept2_hash = hash2
         self.relation = relation
         self.coeff_dict = coeff_dict
 
+
 class get_mapppings(Resource):
     def __init__(self):
-        self.user_ = 'hiic'  # i just put the connect and db access information because I was using a local database
+        # i just put the connect and db access information because I was using a local database
+        self.user_ = 'hiic'
         self.host_ = 'db01.healthcreek.org'
         self.pass_ = 'greenes2018'
         self.db_ = 'derived'
 
-    def get(self, data_json):
-        if data_json is not None:
-            try:
-                conn = sql.connect(user=self.user_, host=self.host_, db=self.db_, passwd=self.pass_)
-                cursor = conn.cursor()
-                subject_list = list()
-                object_list = list()
-                mapping_list = list()
-                concept_dict = dict()
-                json_list = list()
-                #
-                # Save JSON subject list
-                # Save JSON object list
-                #
+    # {'subj_list': [ ... ], 'obj_list': [ ... ]}
+    def get(self):
+        subj_list = request.args.get('subj_list')
+        obj_list = request.args.get('obj_list')
+        table_name = "tmp"
 
-                for n in subject_list:
-                    for m in object_list:
-                        where_query = f"""WHERE subject_code = \'{m.concept_code}\' AND object_code = \'{n.concept_code}\'"""
-                        n = 1
-                        query_row_string = f"""SELECT * FROM <Table_Name> {where_query} LIMIT {n}"""
-                        print(query_row_string)
-                        cursor.execute(query_row_string)
-                        row = cursor.fetchall()
+        if (len(subj_list) == 0) or (len(obj_list) == 0):
+            raise ValueError("Data must have non-zero length")
 
-                        #initialize mapping object
-                        temp = mapping(row[0], row[1], row[2], row[3])
+        try:
+            conn = sql.connect(
+                user=self.user_, host=self.host_, db=self.db_, passwd=self.pass_)
+            cursor = conn.cursor()
+            mapping_list = list()
+            concept_dict = dict()
+            json_list = list()
+            #
+            # Save JSON subject list
+            # Save JSON object list
+            #
 
-                        #insert into mapping list
-                        mapping_list.append(temp)
+            for subj in subj_list:
+                for obj in obj_list:
+                    where_query = f"""WHERE subject_code = \'{subj.concept_code}\' AND object_code = \'{obj.concept_code}\'"""
+                    subj = 1
+                    query_row_string = f"""SELECT * FROM {table_name} {where_query} LIMIT {obj}"""
+                    print(query_row_string)
+                    cursor.execute(query_row_string)
+                    row = cursor.fetchall()
 
-                        #contains all subjects and objects
-                        concept_dict.update({n:m})
+                    # initialize mapping object
+                    temp = mapping(row[0], row[1], row[2], row[3])
 
-                        #return lists as JSON file
-                        json_list.append(mapping_list, concept_dict)
-                        response = Response(json.dumps(json_list), status=200, mimetype='application/json')
-                        return response
+                    # insert into mapping list
+                    mapping_list.append(temp)
 
-            except:
-                return json.dumps({})
+                    # contains all subjects and objects
+                    concept_dict.update({subj: obj})
+
+                    # return lists as JSON file
+                    json_list.append(mapping_list, concept_dict)
+                    response = Response(json.dumps(
+                        json_list), status=200, mimetype='application/json')
+                    return response
+
+        except:
+            return json.dumps({})
