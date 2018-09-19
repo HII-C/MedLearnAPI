e93fe08c4e0eb089b94f559bc1f445a76f8cd78c
James Yu <jamesyu@Jamess-MacBook-Pro.local>
2018-09-19 16:48:32 -0700

    added documentation

--- get_mappings.py
+++ get_mappings.py
@@ -27,12 +27,14 @@ class Relation(Enum):
 
 @dataclass
 class Concept:
+    # setting the types of the attributes of Concept
     type_: ConceptType
     code: str = ""
     system: EncodingSystem
     value: float = 0.0
     units: str = ""
 
+    #constructor for Concept
     def __init__(self, type_, code, system, value, units):
         self.type_ = type_
         self.code = code
@@ -43,11 +45,13 @@ class Concept:
 
 @dataclass
 class Mapping:
+    # setting the types of the attributes of Concept
     c1_hash: str = ""
     c2_hash: str = ""
     relation: Relation
     coeff_dict: Dict = None
 
+    # constructor for Concept
     def __init__(self, hash1, hash2, relation, coeff_dict):
         self.c1_hash = hash1
         self.c2_hash = hash2
@@ -66,7 +70,7 @@ class get_mapppings(Resource):
     cursor: sql.cursors.Cursor = None
 
     def __init__(self, user='hiic', host='db01.healthcreek.org', pw_='greenes2018', db='derived', table_name="tmp"):
-        # i just put the connect and db access information because I was using a local database
+        # the connection to the database only has to occur once therefor, it can occur in the initialization
         self.user = user
         self.host = host
         self.pw_ = pw_
@@ -78,22 +82,28 @@ class get_mapppings(Resource):
 
     # Parameters of form: {'subj_list': [ ... ], 'obj_list': [ ... ]}
     def get(self):
+        # the json objects are recieved via request
         raw_subj_list = request.args['subj_list']
         raw_obj_list = request.args['obj_list']
 
+        # checks to see if the lists for the subj_list and the obj_list are zero
         if (len(raw_subj_list) == 0) or (len(raw_obj_list) == 0):
             raise ValueError("Data must have non-zero length")
 
+        # the result will be ret_dict which is hash_to_mappings over hash_to_concept
         hash_to_mappings: Dict[str, List[Mapping]] = dict()
         hash_to_concept: Dict[str, Concept] = dict()
         ret_dict: Dict[str, dict] = dict()
 
+        # creating a list of subj and obj by iterating the raw_subj_list and passing in the attributes as an argument to the constructor
         subj_list: List[Concept] = [Concept(**subj) for subj in raw_subj_list]
         obj_list: List[Concept] = [Concept(**obj) for obj in raw_obj_list]
 
+        # create the hash to concept dict by utilizing the subj and obj hash as the key the the subj/obj as a value
         for conc in (subj_list + obj_list):
             hash_to_concept[hash(conc)] = conc
 
+        #created tuple of subj with its hash and obj with its hash (hash: 0, sub/obj: 1)
         subj_code_tup: Tuple[Tuple[str, str]] = tuple(
             [tuple([hash(subj), subj.code]) for subj in subj_list])
         obj_code_tup = tuple([[hash(obj), obj.code] for obj in obj_list])
@@ -108,16 +118,21 @@ class get_mapppings(Resource):
                 # assuming: c1 | c2 | rela | gain | boost | bs
                 row = self.cursor.fetchall()
 
+                #stores the coeff_dict in to a dict object
                 coeff_dict = dict()
                 for k, v in self.coeff_dict_form.items():
                     coeff_dict[k] = float(row[v])
 
                 tmp = {"hash1": subj_code[0], "hash2": obj_code[0],
                        "relation": row[2], "coeff_dict": coeff_dict}
+
+                #creates the mappings dict utilizing the subj_code hash as the key and the tmp key values as the values
                 hash_to_mappings[subj_code[0]].extend([Mapping(**tmp)])
 
         ret_dict = {"hash_to_mappings": hash_to_mappings,
                     "hash_to_concept": hash_to_concept}
+
+        #turn the ret_dict in to a json obj
         response = Response(json.dumps(ret_dict), status=200,
                             mimetype='application/json')
         return response
