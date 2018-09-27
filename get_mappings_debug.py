bb36ebb024a0376bf1e4c7c1af4f04f1a93b6d5a
James Yu <jamesyu@jamess-mbp.3520.dhcp.asu.edu>
2018-09-26 17:10:33 -0700

    debugging changes

--- get_mappings_debug.py
+++ get_mappings_debug.py
@@ -1,12 +1,12 @@
 import json
 from dataclasses import dataclass
-from typing import List, T, Dict, Tuple
+from typing import List, Dict, Tuple
 from enum import Enum
 from flask_restful import Resource
 from flask import Flask, Response, request
 from numpy import array
-import MySQLdb as sql
-# import pymysql as sql
+# import MySQLdb as sql
+import pymysql as sql
 
 
 class ConceptType(Enum):
@@ -29,10 +29,10 @@ class Relation(Enum):
 class Concept:
     # setting the types of the attributes of Concept
     type_: ConceptType
-    code: str = ""
+    code: str
     system: EncodingSystem
-    value: float = 0.0
-    units: str = ""
+    value: float
+    units: str
 
     #constructor for Concept
     def __init__(self, type_, code, system, value, units):
@@ -46,10 +46,10 @@ class Concept:
 @dataclass
 class Mapping:
     # setting the types of the attributes of Concept
-    c1_hash: str = ""
-    c2_hash: str = ""
+    c1_hash: str
+    c2_hash: str
     relation: Relation
-    coeff_dict: Dict = None
+    coeff_dict: Dict
 
     # constructor for Concept
     def __init__(self, hash1, hash2, relation, coeff_dict):
@@ -69,68 +69,96 @@ class get_mapppings(Resource):
     conn: sql.connections.Connection = None
     cursor: sql.cursors.Cursor = None
 
-    def __init__(self, user='hiic', host='db01.healthcreek.org', pw_='greenes2018', db='derived', table_name="tmp"):
+    def __init__(self, user='root', pw_='star2222', db='derived', table_name="mappings"):
         # the connection to the database only has to occur once therefor, it can occur in the initialization
         self.user = user
-        self.host = host
         self.pw_ = pw_
         self.db = db
         self.conn = sql.connect(user=self.user, host=self.host,
                                 db=self.db, passwd=self.pw_)
         self.cursor = self.conn.cursor()
         self.table_name = table_name
+        print('connected')
+
 
     # Parameters of form: {'subj_list': [ ... ], 'obj_list': [ ... ]}
     def get(self):
         # the json objects are recieved via request
         raw_subj_list = request.args['subj_list']
+        raw_subj_list = raw_subj_list.split('+')
+        subj_concept_list = list()
+        for x in raw_subj_list:
+            temp_list = x.split(',')
+            temp_concept = Concept(temp_list[0],temp_list[1],temp_list[2],temp_list[3],temp_list[4])
+            subj_concept_list.append(temp_concept)
+        print(subj_concept_list)
         raw_obj_list = request.args['obj_list']
+        raw_obj_list = raw_obj_list.split('+')
+        obj_concept_list = list()
+        for x in raw_obj_list:
+            temp_list = x.split(',')
+            temp_concept = Concept(temp_list[0], temp_list[1], temp_list[2], temp_list[3], temp_list[4])
+            obj_concept_list.append(temp_concept)
+        print(obj_concept_list)
 
         # checks to see if the lists for the subj_list and the obj_list are zero
         if (len(raw_subj_list) == 0) or (len(raw_obj_list) == 0):
             raise ValueError("Data must have non-zero length")
 
         # the result will be ret_dict which is hash_to_mappings over hash_to_concept
-        hash_to_mappings: Dict[str, List[Mapping]] = dict()
-        hash_to_concept: Dict[str, Concept] = dict()
+        hash_to_mappings: Dict[str, dict()] = dict()
+        hash_to_concept: Dict[str, json] = dict()
         ret_dict: Dict[str, dict] = dict()
 
         # creating a list of subj and obj by iterating the raw_subj_list and passing in the attributes as an argument to the constructor
-        subj_list: List[Concept] = [Concept(**subj) for subj in raw_subj_list]
-        obj_list: List[Concept] = [Concept(**obj) for obj in raw_obj_list]
+        #subj_list: List[Concept] = [Concept(**subj) for subj in raw_subj_list]
+        #obj_list: List[Concept] = [Concept(**obj) for obj in raw_obj_list]
 
         # create the hash to concept dict by utilizing the subj and obj hash as the key the the subj/obj as a value
-        for conc in (subj_list + obj_list):
-            hash_to_concept[hash(conc)] = conc
+        for conc in (subj_concept_list + obj_concept_list):
+            hash_to_concept[hash(conc.code)] = (conc.__dict__)
 
         #created tuple of subj with its hash and obj with its hash (hash: 0, sub/obj: 1)
-        subj_code_tup: Tuple[Tuple[str, str]] = tuple(
-            [tuple([hash(subj), subj.code]) for subj in subj_list])
-        obj_code_tup = tuple([[hash(obj), obj.code] for obj in obj_list])
+        subj_code_tup: tuple(tuple[hash, str]) = tuple(tuple([hash(subj.code), subj.code]) for subj in subj_concept_list)
+
+        print(subj_code_tup)
+        obj_code_tup: tuple(tuple[hash, str]) = tuple([tuple([hash(obj.code), obj.code]) for obj in obj_concept_list])
+        print(obj_code_tup)
 
         for subj_code in subj_code_tup:
+            mappings_list: list(dict) = list()
             for obj_code in obj_code_tup:
-                where_query = f"""WHERE subject_code = '{subj_code[1]}' AND object_code in {obj_code_tup[1]}"""
+                print(subj_code[1])
+                print(obj_code[1])
+                where_query = f"""WHERE c1 = '{subj_code[1]}' AND c2 = '{obj_code[1]}'"""
                 exec_str = f"""SELECT * FROM {self.table_name} {where_query}"""
                 print(exec_str)
 
                 self.cursor.execute(exec_str)
                 # assuming: c1 | c2 | rela | gain | boost | bs
                 row = self.cursor.fetchall()
+                print(row)
+                if (len(row) == 0):
+                    next()
+                else:
 
-                #stores the coeff_dict in to a dict object
-                coeff_dict = dict()
-                for k, v in self.coeff_dict_form.items():
-                    coeff_dict[k] = float(row[v])
+                    for x in row:
+                        coefficient: Dict[str, int] = {"gain": x[3]}
+                        tmp_mapping = Mapping(x[0], x[1], x[2], coefficient)
+                        mappings_list.append(tmp_mapping.__dict__)
 
-                tmp = {"hash1": subj_code[0], "hash2": obj_code[0],
-                       "relation": row[2], "coeff_dict": coeff_dict}
 
-                #creates the mappings dict utilizing the subj_code hash as the key and the tmp key values as the values
-                hash_to_mappings[subj_code[0]].extend([Mapping(**tmp)])
 
-        ret_dict = {"hash_to_mappings": hash_to_mappings,
-                    "hash_to_concept": hash_to_concept}
+                    tmp = {"c1_hash": subj_code[0], "c2_hash": obj_code[0],
+                           "relation": [x[2] for x in row], "coeff_dict": [x[3] for x in row]}
+
+                    print(tmp)
+
+
+            hash_to_mappings[subj_code[0]] = mappings_list
+
+        ret_dict = dict({"hash_to_mappings": hash_to_mappings,
+                    "hash_to_concept": hash_to_concept})
 
         #turn the ret_dict in to a json obj
         response = Response(json.dumps(ret_dict), status=200,
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from get_mappings import get_mapppings
+from get_mappings_debug import get_mapppings
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,6 +14,7 @@ def start():
     return """<b>HII-C MedLearn API.</b><br><br>
             This a playground for HII-C MedLearn   
             """
+api.add_resource(get_mapppings, '/get_mappings')
 
 
 app.run(host='0.0.0.0', port='5000')
