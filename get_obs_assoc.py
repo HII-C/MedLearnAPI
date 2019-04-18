d7a5918798647eabc6decd04ab50f970bc705c84
kaylatapia <ktapia2@asu.edu>
2019-04-18 05:34:12 -0700

    removed last commits, tested observation association api call

--- get_obs_assoc.py
+++ get_obs_assoc.py
@@ -8,24 +8,26 @@ from associations.util.concept_util import ConceptType
 from dataclasses import dataclass
 from typing import List, Tuple
 
-@dataclass
-class Concept:
-    # setting the types of the attributes of Concept
-    type_: ConceptType
-    code: str
-    # constructor for Concept
-    def __init__(self, type_, code, system, value, units):
-        self.type_ = type_
-        self.code = code
-        self.system = system
-        self.value = value
-        self.units = units
+# @dataclass
+# class Concept:
+#     # setting the types of the attributes of Concept
+#     type_: ConceptType
+#     code: str
+#
+#     # constructor for Concept
+#     def __init__(self, type_, code, system, value, units):
+#         self.type_ = type_
+#         self.code = code
+#         self.system = system
+#         self.value = value
+#         self.units = units
 
 class get_obs_assoc(Resource):
+
     rel_db: DatabaseHandle = None
 
     def __init__(self, db_params, table_name="ModelStorage"):
-        # def __init__(self, user='root', host='localhost', pw_='', db='derived', table_name="tmp_assoc"):
+    #def __init__(self, user='root', host='localhost', pw_='', db='knowledge', table_name="ModelStorage"):
         # the connection to the database only has to occur once therefor, it can occur in the initialization
         self.host = db_params['host']
         self.db = db_params['db']
@@ -40,36 +42,42 @@ class get_obs_assoc(Resource):
         req_parser.add_argument('obs_list', type=str, action='append')
         args = req_parser.parse_args()
 
-        conc_str = args['concept']
-        raw_obs_list = args['obj_list']
-        obs_list: List[Concept] = [
-            Concept(**json.loads(subj.replace("'", "\""))) for subj in raw_obs_list]
+        conc_str = args['concept'][0]
+        raw_obs_list = args['obs_list']
+        obs_list = list(map(lambda ob: ob.strip(), raw_obs_list[0].split(",")))
 
         if (len(obs_list) == 0):
             raise ValueError("Data must have non-zero length")
 
         # check if the table exists in the first place
         tbl_query = f'''
-                    SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{self.table_name}' 
-                    AND TABLE_SCHEMA = '{db_name}'
-                    '''
+                    SELECT TABLE_NAME FROM {db_name}.TABLES WHERE TABLE_NAME = '{self.table_name}'
+                    AND AND TABLE_SCHEMA = '{db_name}'
+                     '''
         self.rel_db.cursor.execute(tbl_query)
         target_tables: Tuple[Tuple[str]] = self.rel_db.cursor.fetchall()
         print(tbl_query)
         num_target_tables: int = len(target_tables)
 
         # if the number of tables is 0 then a table must be created
+        num_target_tables = 1
         if num_target_tables > 0:
             for obs in obs_list:
                 tbl_query = f'''
-                            SELECT (Concept1, Concept2, Coefficient) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{self.table_name}', 
-                            TABLE_SCHEMA = '{db_name}, Concept1 = {conc_str}, Concept2 = {obs}'
+                            SELECT Concept1, Concept2, Coefficient FROM {db_name}.{self.table_name} WHERE Concept1 = '{conc_str}' AND Concept2 = '{obs}'
                             '''
-                self.rel_db.cursor.execute(tbl_query)
-                ret_tables: Tuple[Tuple[str]] = self.rel_db.cursor.fetchall()
-                ret_list.append(ret_tables)
+                self.rel_db.cursor.execute(tbl_query) #add rel_db back
+                ret_tables: Tuple[Tuple[str]] = self.rel_db.cursor.fetchall() #add rel_db back
+                if ret_list == None:
+                    ret_list = [ret_tables]
+                else:
+                    ret_list.append(ret_tables)
 
         else:
             raise ValueError("Table is empty")
 
+        print(ret_list)
+        response = Response(json.dumps(ret_list), status=200,
+                            mimetype='application/json')
 
+        return response
--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from get_mappings import get_mapppings
 from get_output import get_output
 from get_assoc import get_assoc
 from get_relation import get_relation
+from get_obs_assoc import get_obs_assoc
 from flask_cors import CORS, cross_origin
 
 
@@ -46,15 +47,16 @@ def main():
             """
 
 
-# api.add_resource(get_mapppings, '/get_mappings')
+#api.add_resource(get_mapppings, '/get_mappings', methods=['GET'])
 api.add_resource(get_relation, '/get_relation',
                  methods=['GET'], resource_class_kwargs={"db_params": params, "table_name": "derived"})
 api.add_resource(get_assoc, '/get_assoc',
                  methods=['GET'], resource_class_kwargs={"db_params": params, })
 api.add_resource(get_output, '/get_output',
                  methods=['GET'], resource_class_kwargs={"db_params": params, })
-
+api.add_resource(get_obs_assoc, '/get_obs_assoc',
+                 methods=['GET'])
 
 app.debug = True
 
-app.run(host='0.0.0.0', port='80', threaded=True)
+app.run(host='0.0.0.0', port='5000', threaded=True)
