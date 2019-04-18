d490b80a5fff3b76b1dcb7f0a112351f9b967820
kaylatapia <ktapia2@asu.edu>
2019-04-18 05:29:04 -0700

    took out debugging code accidentally pushed

--- get_obs_assoc.py
+++ get_obs_assoc.py
@@ -7,49 +7,32 @@ from flask_restful import Resource, reqparse
 from associations.util.concept_util import ConceptType
 from dataclasses import dataclass
 from typing import List, Tuple
-import MySQLdb as sql
 
-@dataclass
-class Concept:
-    # setting the types of the attributes of Concept
-    type_: ConceptType
-    code: str
-
-    # constructor for Concept
-    def __init__(self, type_, code, system, value, units):
-        self.type_ = type_
-        self.code = code
-        self.system = system
-        self.value = value
-        self.units = units
+# @dataclass
+# class Concept:
+#     # setting the types of the attributes of Concept
+#     type_: ConceptType
+#     code: str
+#
+#     # constructor for Concept
+#     def __init__(self, type_, code, system, value, units):
+#         self.type_ = type_
+#         self.code = code
+#         self.system = system
+#         self.value = value
+#         self.units = units
 
 class get_obs_assoc(Resource):
-    user: str = ""
-    host: str = ""
-    pw_: str = ""
-    db: str = ""
-    table_name: str = ""
-    conn: sql.connections.Connection = None
-    cursor: sql.cursors.Cursor = None
 
     rel_db: DatabaseHandle = None
 
-    #def __init__(self, db_params, table_name="ModelStorage"):
-    def __init__(self, user='root', host='localhost', pw_='Kikikabob2016!', db='knowledge', table_name="ModelStorage"):
+    def __init__(self, db_params, table_name="ModelStorage"):
+    #def __init__(self, user='root', host='localhost', pw_='', db='knowledge', table_name="ModelStorage"):
         # the connection to the database only has to occur once therefor, it can occur in the initialization
-        #self.host = db_params['host']
-        #self.db = db_params['db']
-        #self.table_name = table_name
-        #self.rel_db = DatabaseHandle(**db_params)
-
-
-        self.user = user
-        self.pw_ = pw_
-        self.db = db
-        self.conn = sql.connect(user=self.user, host=self.host,
-                                db=self.db, passwd=self.pw_)
-        self.cursor = self.conn.cursor()
+        self.host = db_params['host']
+        self.db = db_params['db']
         self.table_name = table_name
+        self.rel_db = DatabaseHandle(**db_params)
 
     def get(self):
         db_name : str = "knowledge"
@@ -62,8 +45,6 @@ class get_obs_assoc(Resource):
         conc_str = args['concept'][0]
         raw_obs_list = args['obs_list']
         obs_list = list(map(lambda ob: ob.strip(), raw_obs_list[0].split(",")))
-        #bs_list: List[Concept] = [
-            #Concept(**json.loads(obs.replace("'", "\""))) for obs in raw_obs_list]
 
         if (len(obs_list) == 0):
             raise ValueError("Data must have non-zero length")
@@ -71,14 +52,14 @@ class get_obs_assoc(Resource):
         # check if the table exists in the first place
 
 
-        # tbl_query = f'''
-        #             SELECT TABLE_NAME FROM {db_name}.TABLES WHERE TABLE_NAME = '{self.table_name}'
-        #             AND AND TABLE_SCHEMA = '{db_name}'
-        #             '''
-        # self.rel_db.cursor.execute(tbl_query)
-        # target_tables: Tuple[Tuple[str]] = self.rel_db.cursor.fetchall()
-        # print(tbl_query)
-        # num_target_tables: int = len(target_tables)
+        tbl_query = f'''
+                    SELECT TABLE_NAME FROM {db_name}.TABLES WHERE TABLE_NAME = '{self.table_name}'
+                    AND AND TABLE_SCHEMA = '{db_name}'
+                     '''
+        self.rel_db.cursor.execute(tbl_query)
+        target_tables: Tuple[Tuple[str]] = self.rel_db.cursor.fetchall()
+        print(tbl_query)
+        num_target_tables: int = len(target_tables)
 
         # if the number of tables is 0 then a table must be created
         num_target_tables = 1
@@ -87,8 +68,8 @@ class get_obs_assoc(Resource):
                 tbl_query = f'''
                             SELECT Concept1, Concept2, Coefficient FROM {db_name}.{self.table_name} WHERE Concept1 = '{conc_str}' AND Concept2 = '{obs}'
                             '''
-                self.cursor.execute(tbl_query) #add rel_db back
-                ret_tables: Tuple[Tuple[str]] = self.cursor.fetchall() #add rel_db back
+                self.rel_db.cursor.execute(tbl_query) #add rel_db back
+                ret_tables: Tuple[Tuple[str]] = self.rel_db.cursor.fetchall() #add rel_db back
                 if ret_list == None:
                     ret_list = [ret_tables]
                 else:
